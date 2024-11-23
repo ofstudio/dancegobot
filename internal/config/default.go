4a10ba6beccc4ead3adb8f5769d2057b5a692e70
Oleg Fomin <ofstudio@gmail.com>
2024-11-23 03:42:54 +0300

    wrap Default into func call

--- internal/config/default.go
+++ internal/config/default.go
@@ -6,29 +6,31 @@ import (
 	tele "gopkg.in/telebot.v4"
 )
 
-// Default is default configuration
-var Default = Config{
+// Default returns default configuration
+func Default() Config {
+	return Config{
 
-	// Bot default configuration
-	Bot: Bot{
-		ApiURL:         tele.DefaultApiURL,
-		UseWebhook:     false,
-		WebhookListen:  ":8080",
-		RPS:            30,
-		Timeout:        10 * time.Second,
-		AllowedUpdates: []string{"message", "inline_query", "chosen_inline_result", "callback_query"},
-	},
+		// Bot default configuration
+		Bot: Bot{
+			ApiURL:         tele.DefaultApiURL,
+			UseWebhook:     false,
+			WebhookListen:  ":8080",
+			RPS:            30,
+			Timeout:        10 * time.Second,
+			AllowedUpdates: []string{"message", "inline_query", "chosen_inline_result", "callback_query"},
+		},
 
-	// Database default configuration
-	DB: DB{
-		RequiredVersion: 1,
-	},
+		// Database default configuration
+		DB: DB{
+			Version: 1,
+		},
 
-	// Application default settings
-	Settings: Settings{
-		EventIDLen:       12,
-		EventTextMaxLen:  2048,
-		DancerNameMaxLen: 64,
-		RendererRepeats:  []time.Duration{2 * time.Second, 10 * time.Second, time.Minute, 1 * time.Hour},
-	},
+		// Application default settings
+		Settings: Settings{
+			EventIDLen:       12,
+			EventTextMaxLen:  2048,
+			DancerNameMaxLen: 64,
+			RendererRepeats:  []time.Duration{2 * time.Second, 10 * time.Second, time.Minute, 1 * time.Hour},
+		},
+	}
 }

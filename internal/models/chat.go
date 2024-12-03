2f7f80371ef59d9f4167f9921829b5e741612c4f
Oleg Fomin <ofstudio@gmail.com>
2024-12-03 15:08:22 +0300

    refactor Chat model

--- internal/models/chat.go
+++ internal/models/chat.go
@@ -1,10 +1,35 @@
 package models
 
+import tele "gopkg.in/telebot.v4"
+
 // Chat is information about a chat where the event announcement was posted.
 type Chat struct {
-	Profile          // Telegram profile
-	Type    ChatType `json:"type"`  // Chat type
-	Title   string   `json:"title"` // Chat title
+	ID       int64    `json:"id"`                 // Chat ID
+	Username string   `json:"username,omitempty"` // Chat username
+	Type     ChatType `json:"type"`               // Chat type
+	Title    string   `json:"title"`              // Chat title
+}
+
+func NewChat(c *tele.Chat) Chat {
+	var t ChatType
+	switch c.Type {
+	case tele.ChatGroup:
+		t = ChatGroup
+	case tele.ChatSuperGroup:
+		t = ChatSuper
+	case tele.ChatChannel:
+		t = ChatChannel
+	case tele.ChatChannelPrivate:
+		t = ChatChannel
+	default:
+		t = ChatPrivate
+	}
+	return Chat{
+		ID:       c.ID,
+		Username: c.Username,
+		Type:     t,
+		Title:    c.Title,
+	}
 }
 
 type ChatType string

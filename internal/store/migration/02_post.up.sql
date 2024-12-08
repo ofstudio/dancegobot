936cbb85203849a4932436fa962ed77ebebb777f
Oleg Fomin <ofstudio@gmail.com>
2024-12-08 13:11:34 +0300

    update migration scripts on #4

--- internal/store/migration/02_post.up.sql
+++ internal/store/migration/02_post.up.sql
@@ -1,12 +1,14 @@
 /*
 AS IS: events.data = '{
   "id": "123abc",
+  "text": "Event announcement",
   "message_id": "ABC123"
   ...
 }'
 
 TO BE: events.data = '{
   "id": "123abc",
+  "caption": "Event announcement",
   post: {
     "inline_message_id": "ABC123"
   }
@@ -18,10 +20,16 @@ TO BE: events.data = '{
 
 -- Step 1: Add the post object field
 UPDATE events
-SET data = json_set(data, '$.post', json_object('inline_message_id', json_extract(data, '$.message_id')))
+SET data = json_remove(
+        json_set(data, '$.post', json_object('inline_message_id', json_extract(data, '$.message_id'))),
+        '$.message_id'
+           )
 WHERE json_extract(data, '$.message_id') IS NOT NULL;
 
--- Step 2: Remove the message_id field
+
+-- Step 2: Rename the text field to caption
 UPDATE events
-SET data = json_remove(data, '$.message_id')
-WHERE json_extract(data, '$.message_id') IS NOT NULL;
+SET data = json_remove(
+        json_set(data, '$.caption', json_extract(data, '$.text')),
+        '$.text')
+WHERE json_extract(data, '$.text') IS NOT NULL;

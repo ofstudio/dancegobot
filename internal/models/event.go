5ec37f12bff171e31ff3bea912b216ca97ee275e
Oleg Fomin <ofstudio@gmail.com>
2024-12-09 18:26:52 +0300

    refactor event settings. add DisableChooseSingle setting

--- internal/models/event.go
+++ internal/models/event.go
@@ -7,16 +7,31 @@ import (
 
 // Event - is a dance event
 type Event struct {
-	ID        string    `json:"id"`               // Random string to identify the event
-	Caption   string    `json:"caption"`          // Event caption
-	Post      *Post     `json:"post"`             // Event post in a Telegram chat
-	Limit     int       `json:"limit,omitempty"`  // Maximum number of couples allowed to sign-in. Zero means no limit
-	Closed    bool      `json:"closed,omitempty"` // Is event closed for new signups or modifications
-	Couples   []Couple  `json:"couples"`          // List of couples signed in
-	Singles   []Dancer  `json:"singles"`          // List of singles signed in
-	Owner     Profile   `json:"owner"`            // Telegram profile of the event owner
-	CreatedAt time.Time `json:"created_at"`       // Creation time
+	ID        string         `json:"id"`         // Random string to identify the event
+	Caption   string         `json:"caption"`    // Event caption
+	Post      *Post          `json:"post"`       // Event post in a Telegram chat
+	Settings  EventsSettings `json:"settings"`   // Event settings
+	Couples   []Couple       `json:"couples"`    // List of couples signed in
+	Singles   []Dancer       `json:"singles"`    // List of singles signed in
+	Owner     Profile        `json:"owner"`      // Telegram profile of the event owner
+	CreatedAt time.Time      `json:"created_at"` // Creation time
 }
+
+// EventSettings - is a settings for the event
+type EventSettings struct {
+	Limit               int       `json:"limit,omitempty"`                 // Maximum number of couples allowed to sign-in. Zero means no limit
+	ClosedFor           ClosedFor `json:"closed_for,omitempty"`            // Is event closed for new signups or modifications
+	DisableChooseSingle bool      `json:"disable_choose_single,omitempty"` // Disable choose specific single dancer from the wait list
+}
+
+type ClosedFor string
+
+const (
+	ClosedForAll             ClosedFor = "all"              // Closed for all. No modifications allowed
+	ClosedForSingles         ClosedFor = "singles"          // Closed for singles
+	ClosedForSingleLeaders   ClosedFor = "single_leaders"   // Closed for single leaders
+	ClosedForSingleFollowers ClosedFor = "single_followers" // Closed for single followers
+)
 
 // EventUpdate is the result of the event update request.
 type EventUpdate struct {

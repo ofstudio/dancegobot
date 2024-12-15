9e533c3e586a4744413eb9a955bb0e0afd04fa47
Oleg Fomin <ofstudio@gmail.com>
2024-12-15 12:19:29 +0300

    update EventService interface

--- internal/telegram/interfaces.go
+++ internal/telegram/interfaces.go
@@ -15,10 +15,10 @@ type EventService interface {
 	NewID() string
 	Create(ctx context.Context, event *models.Event) error
 	Get(ctx context.Context, id string) (*models.Event, error)
-	PostAdd(ctx context.Context, eventID string, inlineMessageID string) (*models.EventUpdate, error)
-	PostChatAdd(ctx context.Context, eventID string, chat *models.Chat, chatMessageID int) (*models.EventUpdate, error)
-	DancerGet(event *models.Event, profile *models.Profile, role models.Role) *models.Dancer
-	CoupleAdd(ctx context.Context, eventID string, profile *models.Profile, role models.Role, other any) (*models.EventUpdate, error)
-	SingleAdd(ctx context.Context, eventID string, profile *models.Profile, role models.Role) (*models.EventUpdate, error)
-	DancerRemove(ctx context.Context, eventID string, profile *models.Profile) (*models.EventUpdate, error)
+	PostAdd(ctx context.Context, eventID string, inlineMessageID string) (*models.Event, *models.Post, error)
+	PostChatAdd(ctx context.Context, eventID string, chat *models.Chat, chatMessageID int) (*models.Event, *models.Post, error)
+	RegistrationGet(event *models.Event, profile *models.Profile, role models.Role) *models.Registration
+	CoupleAdd(ctx context.Context, eventID string, profile *models.Profile, role models.Role, other any) (*models.Registration, error)
+	SingleAdd(ctx context.Context, eventID string, profile *models.Profile, role models.Role) (*models.Registration, error)
+	DancerRemove(ctx context.Context, eventID string, profile *models.Profile) (*models.Registration, error)
 }
